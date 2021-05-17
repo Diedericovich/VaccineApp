d3717cf4b7d0f4b70f55bbd8147cbc7f61a177a1
Impesoft <MrBlubke@yahoo.com>
2021-05-17 17:04:55 +0200

    bootstrap

--- vaccinefront/src/app/app.module.ts
+++ vaccinefront/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+
   ],
   imports: [
     BrowserModule,
