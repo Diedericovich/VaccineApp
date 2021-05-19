ad53d063c6476d0ddc880ddc1894a4e4480efd42
JensVanGelder <jens.v.gelder@gmail.com>
2021-05-19 15:13:27 +0200

    Fixed VaccineRepo Interface Injection

--- VaccineApp/Repositories/IVaccineRepo.cs
+++ VaccineApp/Repositories/IVaccineRepo.cs
@@ -7,7 +7,15 @@ namespace VaccineApp.Repositories
     public interface IVaccineRepo
     {
         Task<List<Vaccine>> GetAllAsync();
+
         Task<List<Vaccine>> GetAllByPartAsync(string part);
+
         Task<Vaccine> GetAsync(int id);
+
+        Task AddAsync(Vaccine vaccine);
+
+        Task DeleteAsync(int id);
+
+        Task UpdateAsync(Vaccine vaccine);
     }
 }
