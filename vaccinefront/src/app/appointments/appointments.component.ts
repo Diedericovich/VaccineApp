e4e2017adbbc8d5a5661a7ec08e22e8f6e7d8c23
Impesoft <MrBlubke@yahoo.com>
2021-05-20 14:49:06 +0200

    status interface update, appointment component ready for canceling appointmens, appointment service updated

--- vaccinefront/src/app/appointments/appointments.component.ts
+++ vaccinefront/src/app/appointments/appointments.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../user';
 import { Appointment } from '../appointment';
+import { AppointmentService } from '../services/appointment.service';
+import { UserService } from '../services/user.service';
 
 
 @Component({
@@ -10,10 +12,13 @@ import { Appointment } from '../appointment';
 })
 export class AppointmentsComponent implements OnInit {
   user?: User ;
-    constructor() { }
+    constructor(private appointmentService : AppointmentService, userService : UserService) { }
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('fullUser')||'{}');
   }
-
+  CancelAppointment(appointmentId: number):void {
+    this.appointmentService.updateAppointmentStatus(appointmentId, 2)
+    //console.log('ok ok, I got here...');
+  }
 }
